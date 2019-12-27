6fee550d723053fdcccd83792fc54d2c3b73ba4e
Jukka Kyrönaho <jugkyron@helsinki.fi>
2019-12-27 19:16:15 +0200

    The first working version 1.0.0

--- server.js
+++ server.js
@@ -1,6 +1,17 @@
 var http = require('http');
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World!\n');
-}).listen(3001, "127.0.0.1");
-console.log('Server running at http://127.0.0.1:3001/');
+var server = http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.write ('<!DOCTYPE html>'+
+    '<html>'+
+      '<head>'+
+          '<meta charset="utf-8" />'+
+          '<title>This is the Nodejs-Example.</title>'+
+      '</head>'+
+      '<body>'+
+          '<p>Hello world!</p>'+
+      '</body>'+
+    '</html>');
+  res.end(' - end - \n');
+});
+server.listen(3000);
+console.log('Server running at http://127.0.0.1:3000/');
